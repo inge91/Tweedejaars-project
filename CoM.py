4396c45afcbf79e72454fa92219e454c10f836af
Inge <inge.becht91@gmail.com>
2012-06-05 15:44:54 +0200

    added an offset dict for the offset between joints

--- CoM.py
+++ CoM.py
@@ -15,10 +15,6 @@ jointCOM = {"RAnkleRoll"      : ( [25.40,  -3.32, -32.41],  0.16175 ),
             "LHipYawPitch"    : ( [-7.66,  -12.00,  27.17 ], 0.07117 ),
             "RElbowRoll"      : ( [65.36,  -0.34, -0.02],  0.18500 ),
             "LElbowRoll"      : ( [65.36,  0.34, -0.02],  0.18500 ),
-            "RWristYaw"       : ( [166.53, -0.83,  4.77],  0.16653 ),
-            "LWristYaw"       : ( [166.53, 0.83,  4.77],  0.16653 ),
-            "RElbowRoll"      : ( [25.56,  -2.73,  0.96],  0.07724),
-            "LElbowRoll"      : ( [25.56,  2.73,  0.96],  0.07724),
             "RElbowYaw"       : ( [-25.60,  0.01, -0.19],  0.05971),
             "LElbowYaw"       : ( [-25.60, - 0.01, -0.19],  0.05971),
             "RShoulderRoll"   : ( [18.85,  -5.77,  0.65],  0.12309),
@@ -30,3 +26,61 @@ jointCOM = {"RAnkleRoll"      : ( [25.40,  -3.32, -32.41],  0.16175 ),
             ######### Misschien niet de juiste versie################
             "HeadPitch"       : ( [1.20,  -0.84,   53.53],  0.52065)}
  
+
+jointOffsets = {
+        ("Torso", "HeadYaw")                : [0.0, 0.0, 126.50 ],
+        ("HeadYaw", "Torso")                : [0.0, 0.0, -126.50 ],
+        ("HeadYaw","HeadPitch")             : [0.0, 0.0, 0.0 ],
+        ("HeadPitch","HeadYaw")             : [0.0, 0.0, 0.0 ],
+        ("Torso", "LShoulderPitch")         : [0.0, 98.0, 100.00 ],
+        ("LShoulderPitch", "Torso")         : [0.0, 98.0, -100.00 ],
+        ("LShoulderPitch", "LShoulderRoll") : [0.0, 0.0, 0.0],
+        ("LShoulderRoll", "LShoulderPitch") : [0.0, 0.0, 0.0],
+        ("LShoulderRoll", "LElbowYaw" )     : [105.00, 15.00, 0.00],        
+        ("LElbowYaw", "LShoulderRoll" )     : [105.00, 15.00, 0.00],        
+        ("LElbowYaw", "LElbowRoll")         : [0, 0, 0],
+        ("LElbowRoll", "LElbowYaw")         : [0, 0, 0],
+        ("LElbowRoll", "LWristYaw")         : [55.95, 0, 0 ],
+        ("LWristYaw", "LElbowRoll")         : [55.95, 0, 0 ],
+        ("Torso", "LHipYawPitch")           : [0, 50.0, -85.0],
+        ("LHipYawPitch", "Torso")           : [0, 50.0, 85.0],
+        ("LHipYawPitch", "LHipRoll")        : [0.0 , 0.0, 0.0],
+        ("LHipRoll", "LHipYawPitch")        : [0.0 , 0.0, 0.0],
+        ("LHipRoll", "LHipPitch")           : [0.0, 0.0, 0.0],
+        ("LHipPitch", "LHipRoll")           : [0.0, 0.0, 0.0],
+        ("LHipPitch", "LKneePitch")         : [0.0, 0.0 -100.0],
+        ("LKneePitch", "LHipPitch")         : [0.0, 0.0 100.0],
+        ("LKneePitch","LAnklePitch")        : [0.0, 0.0, -102.9],
+        ("LAnklePitch","LKneePitch")        : [0.0, 0.0, 102.9],
+        ("LAnklePitch", "LAnkleRoll")       : [0.0, 0.0, 0.0],
+        ("LAnkleRoll", "LAnklePitch")       : [0.0, 0.0, 0.0]
+        ("Torso", "RShoulderPitch")         : [0.0, -98.0, 100.00 ],
+        ("RShoulderPitch", "Torso")         : [0.0, -98.0, -100.00 ],
+        ("RShoulderPitch", "RShoulderRoll") : [0.0, 0.0, 0.0],
+        ("RShoulderRoll", "RShoulderPitch") : [0.0, 0.0, 0.0],
+        ("RShoulderRoll", "RElbowYaw" )     : [105.00, -15.00, 0.00],        
+        ("RElbowYaw", "RShoulderRoll" )     : [105.00, -15.00, 0.00],        
+        ("RElbowYaw", "RElbowRoll")         : [0, 0, 0],
+        ("RElbowRoll", "RElbowYaw")         : [0, 0, 0],
+        ("RElbowRoll", "RWristYaw")         : [55.95, 0, 0 ],
+        ("RWristYaw", "RElbowRoll")         : [55.95, 0, 0 ],
+        ("Torso", "RHipYawPitch")           : [0, -50.0, -85.0],
+        ("RHipYawPitch", "Torso")           : [0, -50.0, 85.0],
+        ("RHipYawPitch", "RHipRoll")        : [0.0 , 0.0, 0.0],
+        ("RHipRoll", "RHipYawPitch")        : [0.0 , 0.0, 0.0],
+        ("RHipRoll", "RHipPitch")           : [0.0, 0.0, 0.0],
+        ("RHipPitch", "RHipRoll")           : [0.0, 0.0, 0.0],
+        ("RHipPitch", "RKneePitch")         : [0.0, 0.0 -100.0],
+        ("RKneePitch", "RHipPitch")         : [0.0, 0.0 100.0],
+        ("RKneePitch","RAnklePitch")        : [0.0, 0.0, -102.9],
+        ("RAnklePitch","RKneePitch")        : [0.0, 0.0, 102.9],
+        ("RAnklePitch", "RAnkleRoll")       : [0.0, 0.0, 0.0],
+        ("RAnkleRoll", "RAnklePitch")       : [0.0, 0.0, 0.0]
+        }
+
+
+               
+
+    
+
+        }
