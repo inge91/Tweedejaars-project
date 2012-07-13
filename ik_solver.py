c83b70710a008ea31f5d2d83c7d4ba40e68ab589
Maarten de Jonge <mdejonge1990@gmail.com>
2012-07-13 16:33:16 +0200

    invert a couple of values in the IK solver
    corrects the positioning of the left leg

--- ik_solver.py
+++ ik_solver.py
@@ -2,18 +2,21 @@ from numpy import matrix
 from numpy.linalg import norm, inv
 from math import cos, sin, pi, acos, atan2, sqrt, asin
 
-def get_angles(foot_to_torso):
+def get_angles(foot_to_torso, leg):
     """ returns a dictionary of joint angles required to reach the given
     foot-position. The footposition is given as a homogenous 4x4 numpy matrix
     that transforms foot-coordinates into torso-coordinates.
-    The calculation is done according to B-Human's analytic solution."""
+    The calculation is done according to B-Human's analytic solution.
+    This leg parameter is either the string LLeg or RLeg. """
+
+    sign = -1 if leg == "LLeg" else 1
 
     angles = {}
 
     # first we define a number of coordinate translations
     # translations are done in millimeters
-    foot_to_hip = trans(y=50) * foot_to_torso
-    foot_to_hip_orth = rotation_x(pi/4.0) * foot_to_hip
+    foot_to_hip = trans(y=50*sign) * foot_to_torso
+    foot_to_hip_orth = rotation_x(pi/4.0, sign) * foot_to_hip
     hiporth_to_foot = inv(foot_to_hip_orth)
 
     # get the knee-angle
@@ -39,7 +42,7 @@ def get_angles(foot_to_torso):
     angles["AnklePitch"] = delta_footpitch
 
     # and here comes the footroll
-    delta_footroll = atan2(foot_to_hip_orth[1, 3], foot_to_hip_orth[2, 3])
+    delta_footroll = atan2(foot_to_hip_orth[1, 3], foot_to_hip_orth[2, 3]) * sign
     angles["AnkleRoll"] = delta_footroll
 
     # now for the other angles
@@ -53,9 +56,9 @@ def get_angles(foot_to_torso):
     rot_component = hiporth_to_thigh[:3, :3]
 
     delta_x = asin(rot_component[2, 1])
-    delta_hipyaw = atan2(-rot_component[0, 1], rot_component[1, 1])
+    delta_hipyaw = atan2(-rot_component[0, 1], rot_component[1, 1]) * -sign
     delta_hippitch = atan2(-rot_component[2, 0], rot_component[2, 2])
-    delta_hiproll = delta_x - (pi / 4.0)
+    delta_hiproll = delta_x - (pi / 4.0) * sign
 
     angles["HipYawPitch"] = delta_hipyaw
     angles["HipPitch"] = delta_hippitch
@@ -73,13 +76,13 @@ def trans(x=0, y=0, z=0):
          [0, 0, 1, z],
          [0, 0, 0, 1]])
 
-def rotation_x(angle):
+def rotation_x(angle, sign=1):
     """ returns a homogenous rotation matrix over the x-axis """
     return matrix(
-        [[1, 0,          0,           0],
-         [0, cos(angle), -sin(angle), 0],
-         [0, sin(angle), cos(angle),  0],
-         [0, 0,          0,           1]])
+        [[1, 0,                  0,                 0],
+         [0, cos(angle),         sin(angle) * sign, 0],
+         [0, sin(angle) * -sign, cos(angle),        0],
+         [0, 0,                  0,                 1]])
 
 def rotation_y(angle):
     """ returns a homogenous rotation matrix over the y-axis """
@@ -109,7 +112,7 @@ def set_position(mp, leg, location, speed):
     location_matrix = (rotation_x(rx) * rotation_y(ry) * rotation_z(rz) *
         trans(x=x, y=y, z=z))
 
-    angles = get_angles(location_matrix)
+    angles = get_angles(location_matrix, leg)
     angles = angles.items()
     leg_prefix = "L" if leg == "LLeg" else "R"
 
