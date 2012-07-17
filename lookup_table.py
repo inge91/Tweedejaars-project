cce70c98ae7fb6270a950de9b0c7ba43d3fb30d3
Inge <inge.becht91@gmail.com>
2012-07-17 14:02:16 +0200

    added lookup table that will calculate all endpositions for legs

---
+++ lookup_table.py
@@ -0,0 +1,11 @@
+def create(leg):
+    # all the ranges in radians
+    LHipYawPitch = (-1.145303, 0.74081)
+    LHipRoll     = (-0.379472, 0.790477)
+    LHipPitch 	 = (-1.773912, 0.484090)
+    LKneePitch 	 = (-0.092346, 2.112528)
+    RHipYawPitch = (-1.145303, 0.74081)
+    RHipRoll 	 = (-0.738321, 0.414754)
+    RHipPitch 	 = (-1.772308 to 0.485624)
+    RKneePitch 	 = (-0.103083 to 2.120198)
+
