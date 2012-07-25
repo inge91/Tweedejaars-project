55c7b9d0d3311269c62df5a889fd2a6067384153
Richard Pronk <Rpronk91@gmail.com>
2012-07-25 03:45:14 +0200

    updated code en verslag update

--- balance.py
+++ balance.py
@@ -1,16 +1,27 @@
 from naoqi import ALProxy
 import time
-import motions
+import os
+import pylab
+import matplotlib.pyplot as plt
 
 class FootBalance():
-    # tweak parameters										best values
-	# supLeg = support leg									left = 1, right = 0
-	# p = sensitivity										0.002
-	# i = Integral sensitivity (not yet used)				1
-	# d = derivative sensitivity							1.2
-	# s = smooting between previous and current value's		0.8
-	# t = threshold when to start balancing					0.004
-	def __init__(self, IP, port, supLeg , p, i, d, s, t):
+    # tweak parameters									best values
+	# supLeg = support leg								left = 1, right = 0
+	# p = sensitivity									0.0015
+	# i = Integral sensitivity (not yet used)			0
+	# d = derivative sensitivity						0.0005
+	# s = smooting between previous and current value's	0.8
+	# tX = threshold on Pitch when to start balancing	2
+	# tY = threshold on Roll when to start balancing	2.5
+	# plot on/off										on = 1,off = 0
+	#
+	#  import balance as b 
+	#  a = b.FootBalance('wind.local',9559,1,0.0015,0,0.0005,0.8,2,2.5,1)
+	#  a.motproxy.setStiffnesses('Body',1)
+	#  a.balance()
+	#  kill met cntl + c
+	#  a.pyPlot()            <- data lists wordt na plotten leeg gemaakt
+	def __init__(self, IP, port, supLeg , p, i, d, s, tX,tY, plot):
 		self.motproxy = ALProxy("ALMotion", IP, port)
 		self.memoryProxy = ALProxy("ALMemory", IP, port)
 		self.supLeg = supLeg
@@ -26,12 +37,23 @@ class FootBalance():
 			self.oldRight = (self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/FrontRight/Sensor/Value") + self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/RearRight/Sensor/Value"))
 		self.KpPitchOld = 0
 		self.KpRollOld = 0
-		self.p = p 
+		self.p = p
 		self.i = i 
 		self.d = d		
-		self.s = s  
-		self.t = t 
-		self.previousTime = time.clock()
+		self.s = s		
+		self.tX = tX
+		self.tY = tY
+		self.plotX = []
+		self.plotY = []
+		self.plotT = []
+		self.plotTx = []
+		self.plotTy = []
+		self.plot = plot
+		self.previousTimeTaken = 0
+		self.smoothedTimetaken = 0
+		self.previousErrorX = 0
+		self.previousErrorY = 0
+		self.previousTime = time.time()
 		
 	def balance(self):
 	    while 1:
@@ -45,26 +67,61 @@ class FootBalance():
 				Back = (1-self.s) * self.oldBack + self.s * (self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/RearLeft/Sensor/Value") + self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/RearRight/Sensor/Value"))
 				Left = (1-self.s) * self.oldFront + self.s * (self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/FrontLeft/Sensor/Value") + self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/RearLeft/Sensor/Value"))
 				Right = (1-self.s) * self.oldFront + self.s * (self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/FrontRight/Sensor/Value") + self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/RearRight/Sensor/Value"))
-			KpPitch = (Front - Back) * self.p
-			KpRoll = (Right - Left) * self.p
-			if(not (KpPitch > self.t or KpPitch < -self.t)):
-				KpPitch = 0
-			if(not (KpRoll > self.t or KpRoll < -self.t)):
-				KpRoll = 0
-			print "KpPitch: " + str(KpPitch)
-			print "KpRoll: " + str(KpRoll)
-			Time = time.clock()
-			timeTaken = self.previousTime - Time
+			
+			Time = time.time()
+			timeTaken = Time - self.previousTime
+			if not(self.smoothedTimetaken == 0 or timeTaken - self.previousTimeTaken < 1):
+				self.smoothedTimetaken = 0.9 * self.smoothedTimetaken + 0.1 * timeTaken 
+			else:
+				self.smoothedTimetaken = timeTaken 
 			self.previousTime = Time
-			KdPitch = ((self.KpPitchOld - KpPitch) / timeTaken) * self.d 
-			KdRoll = ((self.KpRollOld - KpRoll) / timeTaken) * self.d 				
-			self.KpPitchOld = KpPitch
-			self.KpRollOld = KpRoll
-			AngleoffsetHipPitch = KpPitch + KdPitch
-			AngleoffsetHiproll =  KpRoll + KdRoll
+					
+			
+			errorX = (Front - Back)
+			errorY = (Right - Left)
+			
+			KpPitch = errorX * self.p  
+			KpRoll = errorY * self.p 	
+			
+			KdPitch = ((errorX - self.previousErrorX) / self.smoothedTimetaken) * self.d 
+			KdRoll = ((errorY  - self.previousErrorY) / self.smoothedTimetaken) * self.d
+			
+			tX2 = self.tX 
+			tY2 = self.tY 
+					
+			if(not (errorX  > tX2 or errorX  < -tX2)): #error band for X
+				KpPitch = 0
+				KdPitch = 0
+			if(not (errorY   > tY2 or errorY  < -tY2)):  #error band for Y
+				KpRoll = 0
+				KdRoll = 0
+
+			if timeTaken - self.previousTimeTaken < 2.5: #low-pass filter
+				AngleoffsetHipPitch = (KpPitch + KdPitch) * self.smoothedTimetaken
+				AngleoffsetHiproll =  (KpRoll + KdRoll)  * self.smoothedTimetaken
+			else:
+				AngleoffsetHipPitch = 0
+				AngleoffsetHiproll =  0
+				#just for the plot:
+				errorX = 0
+				errorY = 0
+				tX2 = 0
+				tY2 = 0
+				
+			self.previousTimeTaken = timeTaken
+			self.previousErrorX = errorX
+			self.previousErrorX = errorY
+			
+			if self.plot:
+				self.plotX.append(errorX)
+				self.plotY.append(errorY)
+				self.plotT.append(Time)
+				self.plotTx.append(tX2)
+				self.plotTy.append(tY2)
+				
 			if self.supLeg:		
 				try:
-					self.motproxy.changeAngles(["LHipRoll","LHipPitch"], [AngleoffsetHiproll,AngleoffsetHipPitch],0.1)
+					self.motproxy.changeAngles(["LHipRoll","LHipPitch"], [AngleoffsetHiproll,AngleoffsetHipPitch],0.2)
 				except:
 					try:
 						self.motproxy.changeAngles(["LHipRoll","LHipPitch"], [0,AngleoffsetHipPitch],0.2)
@@ -76,7 +133,7 @@ class FootBalance():
 						print "Hiproll out of range"
 			else:
 				try:
-					self.motproxy.changeAngles(["RHipRoll","RHipPitch"], [AngleoffsetHiproll,AngleoffsetHipPitch],0.1)
+					self.motproxy.changeAngles(["RHipRoll","RHipPitch"], [AngleoffsetHiproll,AngleoffsetHipPitch],0.2)
 				except:
 					try:
 						self.motproxy.changeAngles(["RHipRoll","RHipPitch"], [0,AngleoffsetHipPitch],0.2)
@@ -86,7 +143,54 @@ class FootBalance():
 						self.motproxy.changeAngles(["RHipRoll","RHipPitch"], [AngleoffsetHiproll,0],0.2)
 					except:
 						print "Hiproll out of range"				
-			print str(self.memoryProxy.getData("Device/SubDeviceList/LFoot/FSR/FrontLeft/Sensor/Value")) + "\t" + str(self.memoryProxy.getData("Device/SubDeviceList/LFoot/FSR/FrontRight/Sensor/Value")) + "  \t  " + str(self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/FrontLeft/Sensor/Value")) + "\t" +  str(self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/FrontRight/Sensor/Value"))
-			print " "
-			print str(self.memoryProxy.getData("Device/SubDeviceList/LFoot/FSR/RearLeft/Sensor/Value")) + "\t" + str(self.memoryProxy.getData("Device/SubDeviceList/LFoot/FSR/RearRight/Sensor/Value")) + "  \t  " + str(self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/RearLeft/Sensor/Value")) + "\t" + str(self.memoryProxy.getData("Device/SubDeviceList/RFoot/FSR/RearRight/Sensor/Value"))
-			print "Angle offsets: " + str(AngleoffsetHiproll) + "   " + str(AngleoffsetHipPitch)
+	
+	def pyPlot(self):
+		self.plotT = [e - self.plotT[0] for e in self.plotT]
+		self.plotTxMax = self.plotTx
+		self.plotTxMin = [e * -1 for e in self.plotTx]
+		self.plotTyMax = self.plotTy
+		self.plotTyMin = [e * -1 for e in self.plotTy]
+		
+		fig = plt.figure()
+		ax = fig.add_subplot(111)
+
+		values = ax.plot(self.plotT,self.plotX,'-', label = 'measured values')
+		tresMax = ax.plot(self.plotT,self.plotTxMax, '-', label = 'max treshold')
+		tresMin = ax.plot(self.plotT,self.plotTxMin, '-', label = 'min treshold')
+		
+		lns = values+tresMax+tresMin
+		labs = [l.get_label() for l in lns]
+		ax.legend(lns, labs, loc=0, prop={'size':10})
+		
+		ax.grid()
+		ax.set_xlabel("Time in seconds")
+		ax.set_ylabel("Error")
+		ax.set_title("Plot of the error in x direction")
+		
+		pylab.show()		
+		pylab.savefig('plotX.png')
+		
+		fig = plt.figure()
+		ax = fig.add_subplot(111)
+
+		values = ax.plot(self.plotT,self.plotY,'-', label = 'measured values')
+		tresMax = ax.plot(self.plotT,self.plotTyMax, '-', label = 'max treshold')
+		tresMin = ax.plot(self.plotT,self.plotTyMin, '-', label = 'min treshold')
+		
+		lns = values+tresMax+tresMin
+		labs = [l.get_label() for l in lns]
+		ax.legend(lns, labs, loc=0, prop={'size':10})
+		
+		ax.grid()
+		ax.set_xlabel("Time in seconds")
+		ax.set_ylabel("Error")
+		ax.set_title("Plot of the error in y direction")
+		
+		pylab.show()		
+		pylab.savefig('plotY.png')
+		
+		self.plotX = []
+		self.plotY = []
+		self.plotT = []
+		self.plotTx = []
+		self.plotTy = []
